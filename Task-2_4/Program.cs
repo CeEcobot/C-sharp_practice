91884f81323f4771c82f27e57196ff6cea67a8fd
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-03-29 09:15:48 +0200

    Add Task-2_4

---
+++ Task-2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine ("Введите целое число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void Div(int N)
+{
+    if (N % 7 == 0 && N % 23 == 0)
+        Console.WriteLine("да");
+    else
+        Console.WriteLine("нет");
+}
+
+Div (N);
