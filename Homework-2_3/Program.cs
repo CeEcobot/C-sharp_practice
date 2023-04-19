c0c19a543487116beed06732ba337633603b8c95
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-04-19 08:54:29 +0200

    Add Homework-2_3

---
+++ Homework-2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру слева заданного числа,
+// или сообщает, что третьей цифры нет.
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("Вы ввели не трехзначное число");
+        return;
+    }
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()!));
