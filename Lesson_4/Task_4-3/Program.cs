87a004c6cea58ccafa26d9d5ede25ff792004f22
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-04-28 07:21:43 +0200

    Lesson-4

---
+++ Lesson_4/Task_4-3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void massive (int mas)
+{
+    int[] numbers = new int [mas]; // Размер массива mas = 8 чисел
+    for (int i = 0; i < mas; i++)
+    {
+        numbers[i] = new Random().Next(2); // Random не включает верхнюю границу
+        Console.WriteLine(numbers[i]);
+    }
+}
+massive(8);
