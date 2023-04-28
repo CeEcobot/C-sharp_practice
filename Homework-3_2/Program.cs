b4f18d8b3783faed6fb08c39b3cc4b38b9c1f22f
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-04-28 05:23:17 +0200

    Correct Homework-3_2

--- Homework-3_2/Program.cs
+++ Homework-3_2/Program.cs
@@ -6,6 +6,15 @@
 
 double Dis(double x1, double y1, double x2, double y2, double z1, double z2)
 {
-    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2));
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2)),2);
 }
-Console.WriteLine(Dis(3, 6, 8, 2, 1, -7));
+
+double x_1 = double.Parse(Console.ReadLine()!);
+double y_1 = double.Parse(Console.ReadLine()!);
+double z_1 = double.Parse(Console.ReadLine()!);
+double x_2 = double.Parse(Console.ReadLine()!);
+double y_2 = double.Parse(Console.ReadLine()!);
+double z_2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Dis(x_1, y_1, z_1, x_2, y_2, z_2));
+
