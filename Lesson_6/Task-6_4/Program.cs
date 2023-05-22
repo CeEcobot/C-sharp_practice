4e6e5937191cfcbd239ad836e2ee71e1d04cf9f6
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-05-22 06:36:04 +0200

    Add Task-6_4

---
+++ Lesson_6/Task-6_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначи. 
+// первые два числа Фибоначи: 0, 1.
+// Если N = 5 -> 01123
+// Если N = 3 -> 011
+// Если N = 7 -> 0112358
+
+void Fib(int a)
+{
+int festNum = 0;
+int secontNum = 1;
+for (int i = 0; i < a; i++)
+{
+Console.Write(festNum + " ");
+(festNum, secontNum) = (secontNum, festNum + secontNum);
+}
+}
+int A = int.Parse(Console.ReadLine()!);
+Fib(A);
+
