815559550c7ae64114b258e868a58cb7223ceae7
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-05-23 06:10:49 +0200

    Add Homework-6_1

---
+++ Lesson_6/Homework-6_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте сколько чисел больше 0 ввел пользователь.
+// 0,7,8,-2,-2 -> 2
+// -1,-7,567,89,223 -> 3
+
+int PositiveNum(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Число {i}: ");
+        int a = int.Parse(Console.ReadLine()!);
+        if (a > 0) count++;
+    }
+    return count;
+}
+Console.Write("Сколько чисел хотели бы ввести?: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = PositiveNum(a);
+Console.WriteLine($"Количество числе больше 0: {result}");
+
