f6951df17aabf8f850ca7c05a996aba26fbed77f
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-05-27 09:57:10 +0200

    Add Task-7_0

---
+++ Lesson_7/Task-7_0/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив размером m x n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            Console.Write($" {arr[i, j]} ");
+    
+        Console.WriteLine();
+    }
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            arr[i, j] = new Random().Next(from, to);
+        
+    }
+
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+//int[,] mass2 =  CopyMass(mass);
+//Console.WriteLine();
+//Print(mass2);
