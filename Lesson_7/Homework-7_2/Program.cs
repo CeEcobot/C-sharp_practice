b1d002a9c005bbe2dd92dae6a5ab6866262a6355
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-05-31 06:09:12 +0200

    Homework-7_2

---
+++ Lesson_7/Homework-7_2/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же
+// указание, что такого элемента нет.
+
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            Console.Write($" {arr[i, j]} ");
+    
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column]; 
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            arr[i, j] = new Random().Next(from, to); 
+        
+    }
+
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+string SearchNum (int[,]arr, int f, int s)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+     if (f > row || f <= 0 || s > column || s <= 0)
+        return $"{f} {s} -> такого элемента нет в массиве";
+    return $"arr[{f}, {s}] = {arr[f - 1, s - 1]} -> элемент в массиве";
+    }
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+Console.Write("Введите позицию в строке массива: ");
+int first = int.Parse(Console.ReadLine()!);
+Console.Write("Введите позицию в столбце массива: ");
+int second = int.Parse(Console.ReadLine()!);
+
+string answer = SearchNum (mass, first, second);
+Console.WriteLine(answer);
+
