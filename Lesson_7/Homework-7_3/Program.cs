c8065afc8deada4f737610bad7eada39fee1cbb0
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-05-31 06:26:09 +0200

    Homework-7_3

---
+++ Lesson_7/Homework-7_3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифмитическое
+// элементов в каждом столбце.
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            Console.Write($" {arr[i, j]} ");
+    
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column]; 
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            arr[i, j] = new Random().Next(from, to);  
+    }
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+void SearchArithmetic(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+    double res; // обнуление переменной
+
+    for (int i = 0; i < column; i++)
+    {
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
+    }
+}
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+SearchArithmetic (mass);
