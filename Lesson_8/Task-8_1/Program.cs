f5b477303cd2834fdd85bcdf34e5204cf6c12055
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-06-01 06:35:26 +0200

    Add Task-8_1

---
+++ Lesson_8/Task-8_1/Program.cs
@@ -0,0 +1,81 @@
+﻿// https://ru.wikihow.com/%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D1%83
+// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+
+            Console.Write($" {arr[i, j]} ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+
+            arr[i, j] = new Random().Next(from, to);
+
+    }
+
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+bool CheckMatrix(int row, int column)
+{
+    bool result = row != column; // != не равно
+    return result;
+}
+
+string RowToColumn(int[,] arr)
+{
+
+    if (CheckMatrix(arr.GetLength(0), arr.GetLength(1)))
+    return "не возможно выполнить транспорирование матрицы";
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
+        }
+    }
+    Print(arr);
+    return "транспорирование матрицы прошло удачно";
+}
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+string result = RowToColumn(mass);
+Console.WriteLine(result);
+
+
