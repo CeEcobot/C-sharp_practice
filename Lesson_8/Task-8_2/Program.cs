d705e40b28069a482c70ee7b5ca0f1318a8474d9
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-06-01 07:22:57 +0200

    Add Task-8_2

---
+++ Lesson_8/Task-8_2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент
+// входных данных.
+
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            Console.Write($" {arr[i, j]} ");
+    
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column]; 
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            arr[i, j] = new Random().Next(from, to); 
+        
+    }
+
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+int[] FrequencyDict(int[,] arr, int max_num)
+{
+    int[] freq = new int[max_num];
+
+    foreach (int item in arr) freq[item] += 1;
+    return freq;
+}
+
+void PrintMass(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine($"{i} встречается: {arr[i]} раз");
+    Console.WriteLine();
+}
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+
+int[] new_mass = FrequencyDict(mass, stop);
+PrintMass(new_mass);
+
