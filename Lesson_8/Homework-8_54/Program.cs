f1e9969758b0e80cc3807b5c2293614c95bf2b9d
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-06-02 06:15:45 +0200

    Add Homework-8_54

---
+++ Lesson_8/Homework-8_54/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// Блок вывода двумерного массива:
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            Console.Write($" {arr[i, j],3}");
+    
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Блок формирования двумерного массива 
+// (принмает размер и от и до каких числе работаем):
+int[,] FillMass(int row, int column, int from, int to) // [,] - означает двумерный массив
+{
+    int[,] arr = new int[row, column]; 
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        
+            arr[i, j] = new Random().Next(from, to + 1); 
+        
+    }
+
+    return arr;
+}
+
+// ~ РЕШЕНИЕ ~ :
+void MaxMinNum(int[,] arr)
+{
+    int row_size = arr.GetLength(0); // первый параметр row => 0 (свойства массива по ширине)
+    int column_size = arr.GetLength(1); // второй параметр column => 1 (свойства массива по высоте)
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            for (int k = 0; k < column_size - j - 1; k++)
+                if (arr[i, k] < arr[i, k + 1])
+                    (arr[i, k], arr[i, k + 1]) = (arr[i, k + 1], arr[i, k]);
+        }
+    }
+
+}
+
+// Блок ввода данных размерности массива:
+Console.WriteLine("Введите количество строк: ");
+int row_num = int.Parse(Console.ReadLine()!); // размерность
+Console.WriteLine("Введите количество столбцов: ");
+int column_num = int.Parse(Console.ReadLine()!); // с какого числа начинаем
+Console.WriteLine("Диапозон от: ");
+int start = int.Parse(Console.ReadLine()!); // на каком числе заканчиваем -1, тк верхняя граница не ваходит
+Console.WriteLine("Диапозон до: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = FillMass(row_num, column_num, start, stop);
+Print(mass);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+
+MaxMinNum(mass);
+Print(mass);
