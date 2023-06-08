4bd148b4a8526184a3cd67e87a80f9edc0ee9ee6
Efimtsev_Denis <efimtsev.denis@gmail.com>
2023-06-08 07:28:24 +0200

    Add Homework-9_1

---
+++ Lesson_9/Homework-9_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+// ~ РЕШЕНИЕ ~ :
+void IntegersNums(int M, int N)
+{
+    if(M > N) return; // базовое условие работы
+    if(M % 2 == 0)
+    Console.WriteLine($"Четные натуральные числа: {M}");
+    IntegersNums(M+1, N); 
+}
+
+Console.WriteLine("Введите первое число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+// ~ ВЫВОД РЕЗУЛЬТАТА РЕШЕНИЯ ~ :
+
+IntegersNums(M, N);
